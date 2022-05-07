ba4b00cdd80b291f987e845e54e1dbcd4cfa0f88
ngoduyanh <ngoduyanh.chip@gmail.com>
2022-05-07 12:04:37 +0700

    some fixes

--- gen.py
+++ gen.py
@@ -39,7 +39,7 @@ def load_img(ch):
     raise FileNotFoundError(f"Note image for char {ch} not found")
 
 
-def main(args):
+def main():
     parser = argparse.ArgumentParser(
         description="Re:Stage! Prism Step note color generator")
     parser.add_argument("colors", metavar="colors",
@@ -48,15 +48,23 @@ def main(args):
                         help="Output image file")
     parser.add_argument("-bg", "--background", default="#fff",
                         help="Background color, in web hex format, allow both short (#fff) and long (#ff0158) format")
-    parser.add_argument("-pt", "--padding-top", default=2,
+    parser.add_argument("-pt", "--padding-top", default="2",
                         help="Top padding, the empty space will be filled by the background color")
-    parser.add_argument("-pl", "--padding-left", default=3,
+    parser.add_argument("-pl", "--padding-left", default="3",
                         help="Left padding, the empty space will be filled by the background color")
-    parser.add_argument("-pr", "--padding-right", default=3,
+    parser.add_argument("-pr", "--padding-right", default="3",
                         help="Right padding, the empty space will be filled by the background color")
-    parser.add_argument("-pb", "--padding-bottom", default=3,
+    parser.add_argument("-pb", "--padding-bottom", default="3",
                         help="Bottom padding, the empty space will be filled by the background color")
-    args = parser.parse_args(args)
+    parser.add_argument("-s", "--spacing", default="0",
+                        help="Spacing between notes")
+    args = parser.parse_args()
+
+    args.padding_top = int(args.padding_top)
+    args.padding_left = int(args.padding_left)
+    args.padding_right = int(args.padding_right)
+    args.padding_bottom = int(args.padding_bottom)
+    args.spacing = int(args.spacing)
 
     os.environ['SDL_VIDEODRIVER'] = 'dummy'
     pygame.init()
@@ -66,7 +74,8 @@ def main(args):
     imgs = {ch: load_img(ch) for ch in chars}
     count = {ch: args.colors.count(ch) for ch in chars}
     width = sum([imgs[ch].get_width() * count[ch] for ch in chars]) \
-        + args.padding_left + args.padding_right
+        + args.padding_left + args.padding_right \
+        + args.spacing * (len(args.colors) - 1)
     height = max([imgs[ch].get_height() for ch in chars])   \
         + args.padding_top + args.padding_bottom
 
@@ -74,14 +83,14 @@ def main(args):
     pygame.draw.rect(surface, parse_bg_color(
         args.background), (0, 0, width, height))
 
-    x, y = args.padding_left, args.padding_right
+    x, y = args.padding_left, args.padding_top
     for ch in args.colors:
         img = imgs[ch]
         surface.blit(img, (x, y))
-        x += img.get_width()
+        x += img.get_width() + args.spacing
 
     pygame.image.save(surface, args.output)
 
 
 if __name__ == "__main__":
-    main(["rgbrgrb", "out.png"])
+    main()
